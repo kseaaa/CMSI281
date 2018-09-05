21266f24bef5346b9058b7ea749a81cb05f415bc
Kathryn Cole <35353012+kseaaa@users.noreply.github.com>
2018-09-05 16:56:07 -0700

    Upload SourceFile.java

---
+++ Classwork/Classwork02/SourceFile.java
@@ -0,0 +1,35 @@
+/** 
+ * A class to store the name of a file and its contents as Strings.
+ * @author Kathryn Cole
+ * @date September 5, 2018
+ */
+
+
+package copiersupport;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class SourceFile {
+
+    private String fileAsString = "";
+    private String fileName = "";
+    
+    public void SourceFile( String inputFile ) throws IOException {
+        this.fileName = inputFile;
+        BufferedReader lineByLine = new BufferedReader(new FileReader(inputFile));
+        try {
+            String line = lineByLine.readLine();
+            while (line != null) {
+                this.fileAsString += line + "\n";
+                line = lineByLine.readLine();
+            }
+        } finally {
+            lineByLine.close();
+        }
+    }
+
+    public String getFileAsString() {
+        return this.fileAsString;
+    }
+}
