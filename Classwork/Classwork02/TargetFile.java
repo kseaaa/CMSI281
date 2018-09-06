c9299ec731fd4b75cf3544a58c6f73b104af6bef
Kathryn Cole <35353012+kseaaa@users.noreply.github.com>
2018-09-06 15:31:42 -0700

    Fixed TargetFile

--- Classwork/Classwork02/TargetFile.java
+++ Classwork/Classwork02/TargetFile.java
@@ -5,22 +5,21 @@
  */
 
 package copiersupport;
-import java.io.File;
 import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class TargetFile {
 
     private String filePath = "";
  
-    public void TargetFile (String inputFile) {
-        this.filePath = inputFile + ".copy";
-    }
-
-    public void writeToFile( String textToFile ) throws IOException {
-        PrintWriter printText = new PrintWriter( new FileWriter( new File( this.filePath ) ) );
-        printText.print(textToFile);
-        printText.close();
+    public TargetFile( String text, String path ) {
+        try {
+            BufferedWriter bw = new BufferedWriter( new FileWriter( path ) );
+            bw.write( text );
+            bw.close();
+        } catch ( IOException ioe ) {
+            System.out.println("targetfile threw an exception");
+        }
     }
 }
