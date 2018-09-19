c704a6da659632b477b5292aa100927819a0fd75
Kathryn Cole <35353012+kseaaa@users.noreply.github.com>
2018-09-19 13:58:15 -0700

    Upload IntLinkedList

---
+++ Classwork/Classwork04/IntLinkedList.java
@@ -0,0 +1,83 @@
+public class IntLinkedList {
+
+      private Node head;
+      private int  size;
+
+     // the constructor
+      IntLinkedList() {
+         head = null;
+         size = 0;
+      }
+
+      public int getSize() {
+         return size;
+      }
+
+      public void prepend( int dataToAdd ) {
+         Node currentHead = head;
+         head = new Node( dataToAdd );
+         head.next = currentHead;
+         size++;
+      }
+
+      private class Node {
+
+         int data;            // remember this is an IntLinkedList
+         Node next;           // here's the self-referential part
+
+         // constructor
+         Node( int d ) {
+            data = d;
+            next = null;
+         }
+      }
+
+      public Iterator getIteratorAt( int index ) {
+         if( (index > size) || (index < 0) ) {
+            throw new IllegalArgumentException();
+         }
+         Iterator it = new Iterator();
+         while( index > 0 ) {
+            it.next();
+            index--;
+         }
+         return it;
+      }
+
+      public class Iterator {
+         private Node currentNode;
+
+         Iterator() {
+            currentNode = head;
+         }
+
+         public void next() {
+            if( currentNode == null ) {
+               return;
+            } else {
+               currentNode = currentNode.next;
+            }
+         }
+
+         public void insertAt( int index, int dataToAdd ) {
+            Iterator it = getIteratorAt( index );
+            Node nodeToAdd = new Node( dataToAdd );
+            nodeToAdd.next = it.currentNode.next;
+            it.currentNode.next = nodeToAdd;
+        }
+
+         public void removeAt( int index ) {
+            Iterator it = getIteratorAt( index - 1 );
+            it.currentNode.next = it.currentNode.next.next;
+         }
+
+         public boolean hasNext() {
+            return ((currentNode != null) && (currentNode.next != null));
+         }
+
+         public int getCurrentInt() {
+            return currentNode.data;
+         }
+
+      }
+   }
